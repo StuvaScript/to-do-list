b4495793e1def91f0f07269e5ec33e73680d189a
Stu <stumac1000@yahoo.com>
2023-01-03 14:08:02 -0800

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./normalize.css";
+import "./style.css";
+
+console.log("but");
+
+const but = 3;
+if (but === 3) {
+  console.log("hello");
+}
+
+// Will need to change mode to production mode after the project is finished.
+// Read this page => https://webpack.js.org/guides/production/
+
+// Remember to change the source branch on Github in order for your
+// page to be displayed on Github
