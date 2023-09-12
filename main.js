0c9f70f3861d2097ed56526091a698b363a3ef15
Stu <stumac1000@yahoo.com>
2023-09-12 16:51:35 -0700

    Make icon work Trying to import an icon and get it to style. This is proving to be mighty frustrating

--- main.js
+++ main.js
@@ -37,12 +37,16 @@ __webpack_require__.r(__webpack_exports__);
 
 //? **`` Initial functions to be ran
 const todoArray = [];
+
 (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createHeader)();
 const header = document.querySelector('header');
-(0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createToDoItemButton)();
-(0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.createToDoItemButtonLogic)();
+
 (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createSortingDropdown)();
 (0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.sortingAndDisplayOfTasksLogic)();
+
+(0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createToDoItemButton)();
+(0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.createToDoItemButtonLogic)();
+
 (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.displayTodaysDate)();
 
 //* **`` FUNCTIONS ``**
@@ -82,11 +86,15 @@ function findPriorities() {
 
 function goToMainScreen() {
   (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.removeChildrenOfContent)();
-  (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createToDoItemButton)();
-  (0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.createToDoItemButtonLogic)();
+
   (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createSortingDropdown)();
   (0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.sortingAndDisplayOfTasksLogic)();
+
+  (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.createToDoItemButton)();
+  (0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.createToDoItemButtonLogic)();
+
   (0,_modules_dom_manipulation__WEBPACK_IMPORTED_MODULE_0__.displayTodaysDate)();
+
   (0,_modules_event_handler__WEBPACK_IMPORTED_MODULE_1__.taskDisplayLogic)();
 }
 
@@ -204,10 +212,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../index */ "./src/index.js");
 /* harmony import */ var _event_handler__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./event-handler */ "./src/modules/event-handler.js");
-/* harmony import */ var _icons_plus_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../icons/plus.png */ "./src/icons/plus.png");
 
 
-
+// import plus from '../icons/add_circle.svg';
 
 
 
@@ -291,19 +298,40 @@ function displayTodaysDate() {
   dateDiv.innerText = today + ' • today';
   _index__WEBPACK_IMPORTED_MODULE_0__.header.prepend(dateDiv);
 }
+//! *********************************************************************************
+//! *********************************************************************************
+//! *********************************************************************************
+//! *********************************************************************************
+//! *********************************************************************************
 
 //? **`` Creates the initial 'ToDo' button
 function createToDoItemButton() {
-  const newToDoButton = document.createElement('button');
+  const newToDoButton = document.createElement('div');
   // newToDoButton.innerText = 'Create ToDo Item';
   newToDoButton.classList.add('new-todo-button');
   _index__WEBPACK_IMPORTED_MODULE_0__.header.prepend(newToDoButton);
 
-  const newTaskIcon = document.createElement('img');
-  newTaskIcon.setAttribute('src', _icons_plus_png__WEBPACK_IMPORTED_MODULE_2__);
-  newTaskIcon.setAttribute('alt', 'Add Task');
+  // <span class="material-symbols-outlined"> add_circle </span>;
+
+  //* **`` This is the google font code below
+
+  const newTaskIcon = document.createElement('span');
+  newTaskIcon.classList.add('material-symbols-outlined');
+  newTaskIcon.innerText = ' add_circle ';
   newToDoButton.append(newTaskIcon);
+
+  //* **`` This is the import icon code below
+
+  // const newTaskIcon = document.createElement('img');
+  // newTaskIcon.setAttribute('src', plus);
+  // newTaskIcon.setAttribute('alt', 'Add Task');
+  // newToDoButton.append(newTaskIcon);
 }
+//! *********************************************************************************
+//! *********************************************************************************
+//! *********************************************************************************
+//! *********************************************************************************
+//! *********************************************************************************
 
 //? **`` Creates the form that takes all the todo info
 function makeForm() {
@@ -676,16 +704,6 @@ function addNewProjectButtonLogic() {
 }
 
 
-/***/ }),
-
-/***/ "./src/icons/plus.png":
-/*!****************************!*\
-  !*** ./src/icons/plus.png ***!
-  \****************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "7cbbd591e97090f7bb6c.png";
-
 /***/ })
 
 /******/ 	});
@@ -727,18 +745,6 @@ module.exports = __webpack_require__.p + "7cbbd591e97090f7bb6c.png";
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -753,26 +759,6 @@ module.exports = __webpack_require__.p + "7cbbd591e97090f7bb6c.png";
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /************************************************************************/
